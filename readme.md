7214a9235a00aa4fd68384f98fc00575b7cb6c4e
Johny Lopes <jlopes@metatron.com.br>
2019-09-04 10:38:30 -0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Sample Node + typescript
+
+Ref
+- https://medium.com/@oieduardorabelo/node-js-e-typescript-o-como-e-com-testes-7affce2c02a8
---
+++ src/entry.ts
@@ -0,0 +1,6 @@
+import server from './server';
+
+server.listen(3000, () => {
+    console.log(`[SERVER] running at http://localhost:3000`);
+    
+})
